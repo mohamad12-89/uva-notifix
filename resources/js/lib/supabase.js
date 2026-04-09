c9b562fe94e9d0690ff88cd3d97eb6112299f361
Shivaths Karne <karneshivaths@gmail.com>
2026-04-09 13:18:42 -0400

    Fixed frontend loading errors

--- resources/js/lib/supabase.js
+++ resources/js/lib/supabase.js
@@ -9,4 +9,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
   );
 }
 
-export const supabase = createClient(supabaseUrl || "", supabaseAnonKey || "");
+export const supabase = createClient(
+  supabaseUrl || "https://placeholder.supabase.co",
+  supabaseAnonKey || "public-anon-key-placeholder",
+);
